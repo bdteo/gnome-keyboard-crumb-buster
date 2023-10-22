072674965be93f7ea687bec02633e7903a01dc27
Boris D. Teoharov <boristeoharov@gmail.com>
2023-10-23 01:44:27 +0300

    Fixes

--- extension.js
+++ extension.js
@@ -1,78 +1,73 @@
-// Project: crumb-buster
-// Created by: Boris Teoharov
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// SPDX-License-Identifier: GPL-2.0-or-later
-const {GObject, St, GLib} = imports.gi;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+import GLib from 'gi://GLib?version=2.0';
+import GObject from 'gi://GObject';
 
-const GETTEXT_DOMAIN = 'crumb-buster';
-const _ = ExtensionUtils.gettext;
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
+import { QuickToggle, SystemIndicator } from 'resource:///org/gnome/shell/ui/quickSettings.js';
 
-const Indicator = GObject.registerClass(
-  class Indicator extends PanelMenu.Button {
-    _init() {
-      super._init(0.0, _('Keyboard Cleaner'));
+const KeyboardToggle = GObject.registerClass(
+  class KeyboardToggle extends QuickToggle {
+    constructor() {
+      super({
+        title: _('Toggle Keyboard'),
+        iconName: 'input-keyboard-symbolic',
+        toggleMode: true,
+      });
 
-      this.add_child(new St.Icon({
-        icon_name: 'input-keyboard-symbolic',
-        style_class: 'system-status-icon',
-      }));
-
-      let enableItem = new PopupMenu.PopupMenuItem(_('Enable Keyboard'));
-      enableItem.connect('activate', this._enableKeyboard.bind(this));
-      this.menu.addMenuItem(enableItem);
-
-      let disableItem = new PopupMenu.PopupMenuItem(_('Disable Keyboard'));
-      disableItem.connect('activate', this._disableKeyboard.bind(this));
-      this.menu.addMenuItem(disableItem);
+      this.connect('clicked', this._onClick.bind(this));
     }
 
-    _enableKeyboard() {
-      GLib.spawn_command_line_sync('xinput enable [device_id]');
+    _onClick() {
+      if (this.checked)
+        GLib.spawn_command_line_sync('setxkbmap -layout us');
+      else
+        GLib.spawn_command_line_sync('setxkbmap -layout null');
     }
-
-    _disableKeyboard() {
-      GLib.spawn_command_line_sync('xinput disable [device_id]');
-    }
-  });
-
-class Extension {
-  constructor(uuid) {
-    this._uuid = uuid;
-    ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
   }
+);
 
+const KeyboardIndicator = GObject.registerClass(
+  class KeyboardIndicator extends SystemIndicator {
+    constructor() {
+      super();
+
+      this._indicator = this._addIndicator();
+      this._indicator.iconName = 'input-keyboard-symbolic';
+
+      const toggle = new KeyboardToggle();
+      toggle.bind_property('checked',
+        this._indicator, 'visible',
+        GObject.BindingFlags.SYNC_CREATE);
+      this.quickSettingsItems.push(toggle);
+    }
+  }
+);
+
+export default class KeyboardSettingsExtension extends Extension {
   enable() {
-    this._indicator = new Indicator();
-    Main.panel.addToStatusArea(this._uuid, this._indicator);
+    this._indicator = new KeyboardIndicator();
+    Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
   }
 
   disable() {
+    this._indicator.quickSettingsItems.forEach(item => item.destroy());
     this._indicator.destroy();
-    this._indicator = null;
   }
-}
-
-// noinspection JSUnusedGlobalSymbols
-/**
- * @param {object} meta - metadata.json
- */
-// eslint-disable-next-line no-unused-vars
-function init(meta) {
-  return new Extension(meta.uuid);
 }
