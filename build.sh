3360941345cb441c47a48715dc054ca0288a0148
Boris D. Teoharov <boristeoharov@gmail.com>
2023-10-24 01:31:20 +0300

    Created a build setup

--- build.sh
+++ build.sh
@@ -12,7 +12,18 @@ git_files=$(git ls-files)
 # Create the ZIP file only if git_files is not empty
 if [ -z "$git_files" ]; then
   echo "No files tracked by git. Nothing to do."
-else
-  zip -r bdteo-crumb-buster.zip "$git_files"
-  echo "ZIP file created: bdteo-crumb-buster.zip"
+  exit 1
 fi
+
+# Specify the ZIP file name
+zip_name="bdteo-crumb-buster.zip"
+
+# Create an empty ZIP file
+zip $zip_name -@ < /dev/null
+
+# Add each git-tracked file to the ZIP
+for file in $git_files; do
+  zip -u $zip_name "$file"
+done
+
+echo "ZIP file created: $zip_name"
